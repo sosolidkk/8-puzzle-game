8f01f0f9906e5aaf3b15193ca982a79d55bf31c6
Lucas Rodrigues <lucasrodrigues123@hotmail.com.br>
2019-09-29 14:43:02 -0300

    busca gulosa

---
+++ js/buscaGulosa.js
@@ -0,0 +1,53 @@
+function expandirNos(estadoInicial)
+{
+    let estadoAtual = estadoInicial
+
+    while (true)
+    {
+        if (verificarSolucao(estadoAtual, solucao))
+        {
+            mostrarSolucao(estadoAtual)
+            return
+        }
+
+        let valores = estadoAtual.valores
+        let vazioIdx = valores.indexOf(" ")
+
+        visitados.push(estadoAtual)
+
+        for (let i = 0; i < 9; i++)
+        {
+            if (change(i, vazioIdx))
+            {
+                let newVet = valores.slice()
+                newVet[vazioIdx] = newVet[i]
+                newVet[i] = " "
+                let newNo = new Estado(estadoAtual, null, verificarCustoGeral(newVet), newVet)
+                nosGerados++
+
+                if (visitado(newNo) == false)
+                {
+                    fronteira.push(newNo)
+                }
+            }
+        }
+
+        fronteira.sort((a, b) => {
+            return a.custo - b.custo
+        })
+
+        estadoAtual = fronteira[0]
+    }
+}
+
+function buscaGulosa(vet)
+{
+    inicio = new Date()
+    // Criar o estado inicial.
+    let no = new Estado(null, [], 0, vet)
+    fronteira = []
+    visitados = []
+    nosGerados = 0
+
+    expandirNos(no)
+}
